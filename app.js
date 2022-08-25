0c0169f23c8d13b795aa7256bb7bbf3432fa4f62
JoseSalado <saladojosemiguel@gmail.com>
2022-08-24 22:12:10 -0300

    agregue otra forma de resolver el ejercicio.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Forma 1 
+
 let numero1 = Number(prompt("Ingrese un numero"));
 let numero2 =Number(prompt("Ingrese otro numero"));
 
@@ -6,3 +8,14 @@ for(let i = 2; i<=10; i++){
     alert(`(${numero1} + ${numero2}) x ${i} = ${resultado}`);
 
 }
+// otra forma de realizar el ejercicio.
+/*
+let numero1 = Number(prompt("Ingrese un numero"));
+let numero2 =Number(prompt("Ingrese otro numero"));
+
+for(let i = 2; i<=10; i++){
+    let suma = numero1 + numero2;
+    let resultado = suma * i;
+    alert(`(${numero1} + ${numero2}) = ${suma} x ${i} = ${resultado}`);
+
+}*/
